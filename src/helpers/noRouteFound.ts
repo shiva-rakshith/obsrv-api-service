415835af8335271c9106a8d8b455a98e3ee78845
GayathriSrividya <gayathrirajavarapu@sanketika.in>
2022-11-25 12:41:57 +0530

    #0 fix:renamed keys properly

--- src/helpers/noRouteFound.ts
+++ src/helpers/noRouteFound.ts
@@ -1,8 +1,9 @@
 import { Request, Response, NextFunction } from "express"
 import httpStatus from "http-status"
+import constants from "../resources/constants.json";
 
 const routeNotFound = (req: Request, res: Response, next: NextFunction) => {
-    next({statusCode: httpStatus.NOT_FOUND, message: "Route not found" })
+    next({ statusCode: httpStatus.NOT_FOUND, message: constants.ROUTE_NOT_FOUND })
 }
 
 export { routeNotFound }
