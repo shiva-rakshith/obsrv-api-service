0165c44ddd82d13b17f6022e3f0cec732d5dbb64
GayathriSrividya <gayathrirajavarapu@sanketika.in>
2022-11-25 12:24:13 +0530

    #0 feat:updated globalErrorHandler

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,18 @@
-import express, { Application } from 'express'
-import { config } from './config/config'
-import { router } from './routes/router'
-const app: Application = express()
-import globalErrorHandler from './helpers/errorHandler'
-import { routeNotFound } from './helpers/noRouteFound';
-app.use(express.json())
+import express, { Application } from "express";
+import { config } from "./config/config";
+import { router } from "./routes/router";
+const app: Application = express();
+import { ResponseHandler } from "./helpers/response";
+import { routeNotFound } from "./helpers/noRouteFound";
+const globalErrorHandler = ResponseHandler.error;
+app.use(express.json());
 
-app.use('/', router)
-app.use('*', routeNotFound)
-app.use(globalErrorHandler)
+app.use("/", router);
+app.use("*", routeNotFound);
+app.use(globalErrorHandler);
 
 app.listen(config.apiPort, () => {
-    console.log(`listening on port ${config.apiPort}`)
-})
+  console.log(`listening on port ${config.apiPort}`);
+});
 
-export default app
+export default app;
