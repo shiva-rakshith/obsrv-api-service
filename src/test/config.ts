ef3c13782d27f1b86bca8ac86cf32fec8ba60cdf
GayathriSrividya <gayathrirajavarapu@sanketika.in>
2022-11-29 10:57:01 +0530

    #0 feat:config file for test cases

---
+++ src/test/config.ts
@@ -0,0 +1,14 @@
+const config = {
+  apiStatusEndPoint: "/status",
+  apiHealthEndPoint: "/status/health",
+  apiDruidEndPoint: "/druid/v2",
+  apiDruidSqlEndPoint: "/druid/v2/sql/",
+
+  druidHost: "http://localhost",
+  druidPort: 8082,
+  druidEndPoint: "/druid/v2",
+  druidSqlEndPoint: "/druid/v2/sql/",
+  druidStatus: "/status",
+  druidHealth: "/status/health/",
+};
+export { config };
