71c3ed8a7cf900b1b729c820b26b7d9cf4d47d8a
GayathriSrividya <gayathrirajavarapu@sanketika.in>
2022-12-22 10:12:32 +0530

    #0 feat:added JSON file for limits

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -6,63 +6,6 @@ const config = {
   druidHealthEndPoint: process.env.druid_health_end_point || "/status/health",
   druidNativeQueryEndPoint: process.env.druid_native_query_end_point || "/druid/v2",
   druidSqlQueryEndPoint: process.env.druid_sql_query_end_point || "/druid/v2/sql/",
-  requestBodySchemaPath: "/src/configs/requestBodySchema.json",
-  nativeQuerySchemaPath: "/src/configs/nativeQuerySchema.json",
-
-  limits: {
-    common: {
-      maxResultThreshold: 100,
-      maxResultRowLimit: 100,
-    },
-    rules: [
-      {
-        dataSource: "telemetry-events",
-        queryRules: {
-          groupBy: {
-            maxDateRange: 30,
-          },
-          scan: {
-            maxDateRange: 30,
-          },
-          search: {
-            maxDateRange: 30,
-          },
-          timeBoundary: {
-            maxDateRange: 30,
-          },
-          timeseries: {
-            maxDateRange: 30,
-          },
-          topN: {
-            maxDateRange: 30,
-          },
-        },
-      },
-      {
-        dataSource: "summary-events",
-        queryRules: {
-          groupBy: {
-            maxDateRange: 30,
-          },
-          scan: {
-            maxDateRange: 30,
-          },
-          search: {
-            maxDateRange: 30,
-          },
-          timeBoundary: {
-            maxDateRange: 30,
-          },
-          timeseries: {
-            maxDateRange: 30,
-          },
-          topN: {
-            maxDateRange: 30,
-          },
-        },
-      },
-    ],
-  },
 };
 
 export { config };
