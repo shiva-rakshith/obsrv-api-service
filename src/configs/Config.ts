801f8d56f648a0641fe6b5d48a03c90e3012c14c
GayathriSrividya <gayathrirajavarapu@sanketika.in>
2023-03-02 16:39:13 +0530

    #13 fix: renamed clientId to client_id

--- src/configs/Config.ts
+++ src/configs/Config.ts
@@ -12,7 +12,7 @@ export const config = {
     "kafka": {
       "config": {
         "brokers": [`${process.env.kafka_host || 'localhost'}:${process.env.kafka_port || 9092}`],
-        "clientId": process.env.clientId || "obsrv-apis",
+        "clientId": process.env.client_id || "obsrv-apis",
         "retry": {
           "initialRetryTime": process.env.kafka_initial_retry_time ? parseInt(process.env.kafka_initial_retry_time) : 3000,
           "retries": process.env.kafka_retries ? parseInt(process.env.kafka_retries) : 5
