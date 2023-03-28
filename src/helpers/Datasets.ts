8df8db05a4a8b7c1294071ed69b5896a87ae50c6
GayathriSrividya <gayathrirajavarapu@sanketika.in>
2023-03-28 19:47:44 +0530

    #16 fix: Bug Fixes

--- src/helpers/Datasets.ts
+++ src/helpers/Datasets.ts
@@ -13,7 +13,6 @@ export class Datasets {
     private data_schema: object
     private router_config: RouterConfig
     private denorm_config: DenormConfig
-    private client_state: object
     private status: string
     private version: string
     private created_by: string
@@ -30,7 +29,6 @@ export class Datasets {
         this.data_schema = payload.data_schema
         this.router_config = payload.router_config
         this.denorm_config = payload.denorm_config
-        this.client_state = payload.client_state
         this.status = payload.status
         this.version = payload.version
         this.created_by = payload.created_by
@@ -39,7 +37,7 @@ export class Datasets {
     }
 
     public getValues() {
-        return Object.assign(this.removeNullValues({ id: this.id, dataset_name: this.dataset_name, dataset_slug: this.dataset_slug, validation_config: this.validation_config, extraction_config: this.extraction_config, dedup_config: this.dedup_config, data_schema: this.data_schema, router_config: this.router_config, denorm_config: this.denorm_config, client_state: this.client_state, status: this.status, version: this.version, created_by: this.created_by, updated_by: this.updated_by, published_date: this.published_date }), { "updated_date": new Date })
+        return Object.assign(this.removeNullValues({ id: this.id, dataset_name: this.dataset_name, dataset_slug: this.dataset_slug, validation_config: this.validation_config, extraction_config: this.extraction_config, dedup_config: this.dedup_config, data_schema: this.data_schema, router_config: this.router_config, denorm_config: this.denorm_config, status: this.status, version: this.version, created_by: this.created_by, updated_by: this.updated_by, published_date: this.published_date }), { "updated_date": new Date })
     }
 
     public setValues() {
--- src/test/IngestorTestService.spec.ts
+++ src/test/IngestorTestService.spec.ts
@@ -1,107 +1,72 @@
-// import app from "../app";
-// import chai from "chai";
-// import chaiHttp from "chai-http";
-// import spies from "chai-spies";
-// import httpStatus from "http-status";
-// import { TestDataIngestion } from "./Fixtures";
-// import { config } from "./Config";
-// import { routesConfig } from "../configs/RoutesConfig";
-// import constants from "../resources/Constants.json"
-// import { ingestorService, kafkaConnector } from "../routes/Router";
+import app from "../app";
+import chai from "chai";
+import chaiHttp from "chai-http";
+import spies from "chai-spies";
+import httpStatus from "http-status";
+import { TestDataIngestion } from "./Fixtures";
+import { config } from "./Config";
+import { routesConfig } from "../configs/RoutesConfig";
+import constants from "../resources/Constants.json"
+import { ingestorService, kafkaConnector } from "../routes/Router";
 
-// chai.use(spies);
-// chai.should();
-// chai.use(chaiHttp);
+chai.use(spies);
+chai.should();
+chai.use(chaiHttp);
 
-// describe("DATA INGEST API", () => {
-//     it("successfully arrange connection to kafka", (done) => {
-//         chai.spy.on(kafkaConnector, "connect", () => {
-//             return Promise.resolve("kafka connection arranged succesfully")
-//         })
-//         kafkaConnector.connect()
-//         ingestorService.init()
-//         chai.expect(kafkaConnector.connect).to.be.called
-//         chai.spy.restore(kafkaConnector, "connect")
-//         done()
-//     });
-//     it("it should not ingest data if service is down", (done) => {
-//         chai.spy.on(kafkaConnector, "connect", () => {
-//             return Promise.reject(new Error("error connecting kafka service"))
-//         })
-//         kafkaConnector.connect()
-//         ingestorService.init()
-//         chai
-//             .request(app)
-//             .post(config.apiDatasetIngestEndPoint)
-//             .send(TestDataIngestion.SAMPLE_INPUT)
-//             .end((err, res) => {
-//                 res.should.have.status(httpStatus.INTERNAL_SERVER_ERROR);
-//                 res.body.responseCode.should.be.eq(httpStatus["500_NAME"]);
-//                 res.body.params.status.should.be.eq(constants.STATUS.FAILURE)
-//                 chai.spy.restore(kafkaConnector, "connect")
-//                 done();
-//             });
-//     });
-//     it("it should ingest data successfully", (done) => {
-//         chai.spy.on(kafkaConnector, "execute", () => {
-//             return Promise.resolve("data ingested")
-//         })
-//         chai
-//             .request(app)
-//             .post(config.apiDatasetIngestEndPoint)
-//             .send(TestDataIngestion.SAMPLE_INPUT)
-//             .end((err, res) => {
-//                 res.should.have.status(httpStatus.OK);
-//                 res.body.should.be.a("object");
-//                 res.body.responseCode.should.be.eq(httpStatus["200_NAME"]);
-//                 res.body.should.have.property("result");
-//                 res.body.id.should.be.eq(routesConfig.data_ingest.api_id);
-//                 res.body.params.status.should.be.eq(constants.STATUS.SUCCESS)
-//                 chai.spy.restore(kafkaConnector, "execute")
-//                 done();
-//             });
-//     });
-//     it("it should not ingest data when datasetid param is empty", (done) => {
-//         chai
-//             .request(app)
-//             .post("/obsrv/v1/data/ /")
-//             .send(TestDataIngestion.SAMPLE_INPUT)
-//             .end((err, res) => {
-//                 res.should.have.status(httpStatus.INTERNAL_SERVER_ERROR);
-//                 res.body.should.be.a("object");
-//                 res.body.responseCode.should.be.eq(httpStatus["500_NAME"]);
-//                 res.body.should.have.property("result");
-//                 res.body.id.should.be.eq(routesConfig.data_ingest.api_id);
-//                 res.body.params.status.should.be.eq(constants.STATUS.FAILURE)
-//                 done();
-//             });
-//     });
-//     it("should throw an error if kafka service is down", (done) => {
-//         chai.spy.on(kafkaConnector, "connect", () => {
-//             return Promise.reject(new Error("error connecting kafka service"))
-//         })
-//         kafkaConnector.connect()
-//         ingestorService.init()
-//         chai.expect(kafkaConnector.connect).to.be.called
-//         chai.spy.restore(kafkaConnector, "connect")
-//         done()
-//     });
-//     it("should close connection to kafka", (done) => {
-//         chai.spy.on(kafkaConnector.producer, "disconnect", () => {
-//             return Promise.resolve("kafka disconnected")
-//         })
-//         kafkaConnector.close()
-//         chai.expect(kafkaConnector.producer.disconnect).to.not.throw
-//         chai.spy.restore(kafkaConnector.producer, "disconnect")
-//         done()
-//     })
-//     it("should throw error while disconnectig kafka", (done) => {
-//         chai.spy.on(kafkaConnector.producer, "disconnect", () => {
-//             return Promise.reject(new Error("failed to disconnect kafka"))
-//         })
-//         kafkaConnector.close()
-//         chai.expect(kafkaConnector.producer.disconnect).to.throw
-//         chai.spy.restore(kafkaConnector.producer, "disconnect")
-//         done()
-//     })
-// })
+describe("DATA INGEST API", () => {
+    it("it should ingest data successfully", (done) => {
+        chai.spy.on(kafkaConnector, "execute", () => {
+            return Promise.resolve("data ingested")
+        })
+        chai
+            .request(app)
+            .post(config.apiDatasetIngestEndPoint)
+            .send(TestDataIngestion.SAMPLE_INPUT)
+            .end((err, res) => {
+                res.should.have.status(httpStatus.OK);
+                res.body.should.be.a("object");
+                res.body.responseCode.should.be.eq(httpStatus["200_NAME"]);
+                res.body.should.have.property("result");
+                res.body.id.should.be.eq(routesConfig.data_ingest.api_id);
+                res.body.params.status.should.be.eq(constants.STATUS.SUCCESS)
+                chai.spy.restore(kafkaConnector, "execute")
+                done();
+            });
+    });
+    it("it should ingest data successfully", (done) => {
+        chai.spy.on(kafkaConnector, "execute", () => {
+            return Promise.reject("error connecting to kafka")
+        })
+        chai
+            .request(app)
+            .post(config.apiDatasetIngestEndPoint)
+            .send(TestDataIngestion.SAMPLE_INPUT)
+            .end((err, res) => {
+                res.should.have.status(httpStatus.INTERNAL_SERVER_ERROR);
+                res.body.should.be.a("object");
+                res.body.responseCode.should.be.eq(httpStatus["500_NAME"]);
+                res.body.should.have.property("result");
+                res.body.id.should.be.eq(routesConfig.data_ingest.api_id);
+                res.body.params.status.should.be.eq(constants.STATUS.FAILURE)
+                chai.spy.restore(kafkaConnector, "execute")
+                done();
+            });
+    });
+    it("it should not ingest data when datasetid param is empty", (done) => {
+        chai
+            .request(app)
+            .post("/obsrv/v1/data/ /")
+            .send(TestDataIngestion.SAMPLE_INPUT)
+            .end((err, res) => {
+                res.should.have.status(httpStatus.INTERNAL_SERVER_ERROR);
+                res.body.should.be.a("object");
+                res.body.responseCode.should.be.eq(httpStatus["500_NAME"]);
+                res.body.should.have.property("result");
+                res.body.id.should.be.eq(routesConfig.data_ingest.api_id);
+                res.body.params.status.should.be.eq(constants.STATUS.FAILURE)
+                done();
+            });
+    });
+    
+    
+})
