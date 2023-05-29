026bca0d7ca26e253782f3ccf1ebc3b3beba1ab7
GayathriSrividya <gayathrirajavarapu@sanketika.in>
2023-05-29 12:13:42 +0530

    feat: added descriptions for default configurations

--- src/configs/Config.ts
+++ src/configs/Config.ts
@@ -1,4 +1,4 @@
-
+// These configurations provide settings and values for various aspects of dataset management, data ingestion, and table configurations in a system.
 export const config = {
   "env": process.env.system_env || "local",
   "api_port": process.env.api_port || 3000,
@@ -32,7 +32,7 @@ export const config = {
     filename: process.env.telemetry_file_filename || 'telemetry-%DATE%.log',
     maxSize: process.env.telemetry_file_maxsize || '100m',
     maxFiles: process.env.telemetry_file_maxfiles || '100',
-    "kafka": {
+    "kafka": {    // The default Kafka configuration includes essential parameters such as broker IP addresses and other configuration options.
       "config": {
         "brokers": [`${process.env.kafka_host || 'localhost'}:${process.env.kafka_port || 9092}`],
         "clientId": process.env.client_id || "obsrv-apis",
@@ -42,7 +42,7 @@ export const config = {
         },
         "connectionTimeout": process.env.kafka_connection_timeout ? parseInt(process.env.kafka_connection_timeout) : 5000
       },
-      "topics": {
+      "topics": {  // Default Kafka topics depend on type of dataset.
         "createDataset": `${process.env.system_env || 'local'}.ingest`,
         "createMasterDataset": `${process.env.system_env || 'local'}.masterdata.ingest`
       }
@@ -56,29 +56,29 @@ export const config = {
     "redis_host": process.env.redis_host || 'obsrv-redis-headless.redis.svc.cluster.local',
     "redis_port": process.env.redis_port || 6379
   },
-  "exclude_datasource_validation": process.env.exclude_datasource_validation || ["system-stats", "failed-events-summary", "masterdata-system-stats"],
+  "exclude_datasource_validation": process.env.exclude_datasource_validation || ["system-stats", "failed-events-summary", "masterdata-system-stats"], // list of datasource names to skip validation while calling query API
   "telemetry_dataset": process.env.telemetry_dataset || "telemetry",
-  "table_names": {
+  "table_names": {     // Names of all tables available for CRUD operations
     "datasets": "datasets",
     "datasources": "datasources",
     "datasetSourceConfig": "dataset_source_config",
     "datasetTransformations": "dataset_transformations"
   },
-  "table_config": {
+  "table_config": {   // This object defines the configuration for each table.
     "datasets": {
-      "pk": "id",
+      "primary_key": "id",
       "references": []
     },
     "datasources": {
-      "pk": "id",
+      "primary_key": "id",
       "references": []
     },
     "dataset_source_config": {
-      "pk": "id",
+      "primary_key": "id",
       "references": []
     },
     "dataset_transformations": {
-      "pk": "id",
+      "primary_key": "id",
       "references": []
     }
   }
