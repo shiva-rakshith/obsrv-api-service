ea6ea9cdb7ad53b4690e5028328b4f2d97d58154
Praveen <praveen.veleneni@gmail.com>
2023-07-17 17:29:52 +0530

    #48 feat: set query service to app state

--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,26 @@ import { config } from "./configs/Config";
 import { ResponseHandler } from "./helpers/ResponseHandler";
 import { loadExtensions } from "./managers/Extensions";
 import { router } from "./routes/Router";
- import bodyParser from "body-parser";
+import bodyParser from "body-parser";
 import { processTelemetryAuditEvent } from "./services/telemetry";
+import { queryService } from "./routes/Router";
+import { routesConfig } from "./configs/RoutesConfig";
+import { QueryValidator } from "./validators/QueryValidator";
 const app: Application = express();
+const queryValidator = new QueryValidator();
+
+const services = {
+  queryService,
+  validationService: queryValidator,
+  nativeQueryId: routesConfig.query.native_query.api_id,
+  sqlQueryId: routesConfig.query.sql_query.api_id,
+}
  
 app.use(bodyParser.json({ limit: config.body_parser_limit}));
+app.use(express.text());
 app.use(express.json());
- 
+app.set("queryServices", services);
+
 loadExtensions(app)
   .finally(() => {
     // app.use(processTelemetryAuditEvent()) // uncomment if above extension is not loaded
@@ -19,8 +32,8 @@ loadExtensions(app)
 
     app.listen(config.api_port, () => {
       console.log(`listening on port ${config.api_port}`);
-    });
   });
+});
 
 
 export default app;
