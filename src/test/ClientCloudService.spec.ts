8d2ca2af068e72123d310fe143737ce3425dd01a
GayathriSrividya <gayathrirajavarapu@sanketika.in>
2023-08-07 12:06:15 +0530

    Fix: Bug Fix

--- src/test/ClientCloudService.spec.ts
+++ src/test/ClientCloudService.spec.ts
@@ -5,7 +5,7 @@ import httpStatus from "http-status";
 import { TestExhaust } from "./Fixtures";
 import { config } from "./Config";
 import constants from "../resources/Constants.json";
-import { exhaustService, globalCache } from "../routes/Router";
+import { datasetService, dbConnector, exhaustService, globalCache, ingestorService } from "../routes/Router";
 import chaiSpies from 'chai-spies'
 import { describe, it } from 'mocha';
 import moment from "moment";
@@ -26,7 +26,7 @@ describe("AWS Cloud Storage", () => {
 
     it("it should return 404 if dataset record not found", (done) => {
         chai.spy.restore()
-        chai.spy.on(globalCache, "get", () => {
+        chai.spy.on(dbConnector, "listRecords", () => {
             return []
         })
         chai
